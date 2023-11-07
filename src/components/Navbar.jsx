3134fc4f79e0460f1b7e752b8827c093fa92ef49
BlaizDickson <bakeungdickson@gmail.com>
2023-11-08 00:49:25 +0100

    add corrections for toggle menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
 
   return (
     <nav
-      className={'${styles.paddingx} w-full flex items-center py-5 fixed top-0 z-20 bg-primary'}
+      className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
     >
       <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
         <Link
@@ -22,8 +22,9 @@ const Navbar = () => {
             window.scrollTo(0,0);
           }}
           >
-            <img src={logo} width="150" height="150" alt="logo"
-            className='w-9 h-9 object-contain' />
+            <img src={logo} 
+                 alt="logo"
+                 className='w-9 h-9 object-contain' />
             <p className='text-white text-[18px] font-bold cursor-pointer'>Bakeung Dickson</p>
         </Link>
         <ul className='list-none hidden sm:flex flex-row gap-10'>
